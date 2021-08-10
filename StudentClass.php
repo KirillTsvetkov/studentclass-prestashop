e69ac7e8420fbdd6f0c1802983dc7188419c5690
KirillTsvetkov <56355589+KirillTsvetkov@users.noreply.github.com>
2021-08-10 11:54:07 +0300

    first commit

---
+++ StudentClass.php
@@ -0,0 +1,47 @@
+<?php
+
+class StudentClass extends ObjectModel
+{
+    public $id;
+
+    public $name;
+
+    public $bdate;
+
+    public $status;
+
+    public $avgscore;
+
+    public static $definition = array(
+        'table' => 'student',
+        'primary' => 'id_student',
+        'multilang' => true,
+        'fields' => array(
+            'name' => array('type' => self::TYPE_STRING, 'validate' => 'isString'),
+            'bdate' => array('type' => self::TYPE_DATE),
+            'status' => array('type' => self::TYPE_BOOL),
+            'avgscore' => array ('type' => self::TYPE_FLOAT)
+        ),
+      );
+
+
+    public function getAll()
+    {
+        $sql = 'SELECT * FROM `' . _DB_PREFIX_ . self::$definition['table'] . '`';
+        return Db::getInstance()->executeS($sql);
+    }
+
+    public function getTopStudent()
+    {
+        $sql = 'SELECT * FROM `' . _DB_PREFIX_ . self::$definition['table'] . '` 
+        WHERE avgscore = (SELECT MAX(avgscore) FROM `' . _DB_PREFIX_ . self::$definition['table'] . '`)';
+        return Db::getInstance()->getValue($sql);
+    }
+
+    public function getTopAvgScore()
+    {
+        $sql = 'SELECT MAX(avgscore) FROM `' . _DB_PREFIX_ . self::$definition['table'] . '` ';
+        return Db::getInstance()->getValue($sql);
+    }
+
+}
